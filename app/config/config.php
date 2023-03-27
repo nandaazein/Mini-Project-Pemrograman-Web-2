1c923a2008348a7906549dfde6af2faf98de713e
MeydaWikaromah <meydaawikaromah@gmail.com>
2023-03-27 14:15:55 +0700

    commit config

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('base_url', 'http://localhost/pegadaian/public');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'meyda11');
+define('DB_NAME', 'pegadaian');
